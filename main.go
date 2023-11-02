ede23f8e399b2916924e9460e6643258d78e6419
Martin Polese <mpolese@fi.uba.ar>
2023-11-02 19:10:28 -0300

    Se limpia un poco el código

--- main.go
+++ main.go
@@ -4,15 +4,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"os"
 
 	"github.com/gocolly/colly"
 )
 
 type Product struct {
-	Name  string //`json:"name"`
-	Price string //`json:"price"`
-	Url   string //`json:"url"`
+	Name  string `json:"name"`
+	Price string `json:"price"`
+	Url   string `json:"url"`
 }
 
 func main() {
@@ -25,23 +24,15 @@ func main() {
 	var visitUrl string = "https://listado.mercadolibre.com.ar/" + productToSearch
 	products := scrapData(visitUrl)
 
-	for _, product := range products {
-		fmt.Println("Nombre:", product.Name)
-		fmt.Println("Precio:", product.Price)
-		fmt.Println("Url:", product.Url)
-	}
-
-	saveAsJsonFile(products) // Guardamos los datos en un archivo
+	//saveAsJsonFile(products) // Guardamos los datos en un archivo
 
 	raiseServer(products)
 }
 
-// Scrapea datos, y devuelve los productos
+// Scrapea datos de una url dada, y devuelve los productos
 func scrapData(url string) []Product {
-	// Crea una nueva instancia de Colly
-	c := colly.NewCollector()
+	c := colly.NewCollector() // Crea una nueva instancia de Colly Collector
 
-	// Slice para almacenar los productos scrapeados
 	var products []Product
 
 	// Se define el comportamiento al scrapear
@@ -55,30 +46,27 @@ func scrapData(url string) []Product {
 		products = append(products, product)
 	})
 
-	// Se visita el sitio a scrapear
-	c.Visit(url)
+	c.Visit(url) // Se visita el sitio a scrapear
 
 	return products
 }
 
-// Guarda un slice de productos como un archivo .json
-func saveAsJsonFile(products []Product) {
-	data, err := json.MarshalIndent(products, "", " ")
-	if err != nil {
-		panic(err)
-	}
+// Escribe los productos recibidos en un archivo, en formato JSON
+// func saveAsJsonFile(products []Product) {
+// 	jsonData, err := json.MarshalIndent(products, "", " ")
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
-	os.WriteFile("products.json", data, 0644)
-}
+// 	os.WriteFile("products.json", jsonData, 0644)
+// }
 
 // Levanta un servidor, en el puerto 8080 que de momento devuelve todos los productos scrapeados al
 // hacer una peticion GET en el endpoint "/""
 func raiseServer(products []Product) {
-	// Levantamos un servidor para dar respuestas
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(products)
-		//fmt.Fprintf(w, "Test")
 	})
 
 	srv := http.Server{
@@ -86,7 +74,6 @@ func raiseServer(products []Product) {
 	}
 
 	err := srv.ListenAndServe()
-
 	if err != nil {
 		panic(err)
 	}
