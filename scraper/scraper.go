8f278c462a15cfcb196316f5f4435a9372acc6b4
Lucas <lgrati@fi.uba.ar>
2023-11-17 16:42:31 -0300

    query params fullh4rd

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -3,6 +3,7 @@ package scraper
 import (
 	"fmt"
 	"go-scraper/utils"
+	"strconv"
 	"strings"
 
 	"github.com/gocolly/colly"
@@ -78,9 +79,22 @@ func applyScrapSettingsMercadoLibre(url string, scrapSettings *utils.Settings) s
 }
 
 // Scrapea datos de fullh4rd, a partir de una url, y devuelve los productos
-func ScrapFullH4rd(url string) []utils.Product {
+func ScrapFullH4rd(url string, scrapSettings *utils.Settings) []utils.Product {
 	c := colly.NewCollector() // Crea una nueva instancia de Colly Collector
 	var products []utils.Product
+	// Se aplican los settings/filtros de scrapeo
+	visitUrl := applyScrapSettingsFullH4rd(url, scrapSettings)
+
+	minPrice, err := strconv.ParseFloat(scrapSettings.MinPrice, 64)
+	if err != nil {
+		minPrice = 0
+
+	}
+
+	maxPrice, err := strconv.ParseFloat(scrapSettings.MaxPrice, 64)
+	if err != nil {
+		maxPrice = 0
+	}
 
 	// Se define el comportamiento al scrapear
 	c.OnHTML("div[class='item product-list']", func(e *colly.HTMLElement) {
@@ -96,12 +110,77 @@ func ScrapFullH4rd(url string) []utils.Product {
 			Url:   "https://www.fullh4rd.com.ar/" + e.ChildAttr("a", "href"),
 		}
 
-		products = append(products, product)
+		if maxPrice > 0 || minPrice > 0 {
+			if maxPrice != 0 && minPrice == 0 {
+				if product.Price < int(maxPrice) {
+					products = append(products, product)
+				}
+			} else if maxPrice == 0 && minPrice != 0 {
+				if product.Price > int(minPrice) {
+					products = append(products, product)
+				}
+			} else if maxPrice != 0 && minPrice != 0 {
+				if product.Price > int(minPrice) && product.Price < int(maxPrice) {
+					products = append(products, product)
+				}
+
+			}
+
+		} else {
+			products = append(products, product)
+		}
+
 	})
 
-	c.Visit(url) // Se visita el sitio a scrapear
+	c.Visit(visitUrl) // Se visita el sitio a scrapear
 
 	return products
+}
+
+// Funcion auxiliar, aplica los settings de busqueda sobre una url para mercadolibre y devuelve
+// una nueva url
+func applyScrapSettingsFullH4rd(url string, scrapSettings *utils.Settings) string {
+	appendStr := "%20"
+
+	// Se aplican los settings para scrapear
+	if scrapSettings.Ram != "" {
+		url += fmt.Sprintf("%s%sgb", appendStr, scrapSettings.Ram)
+	}
+
+	if scrapSettings.Storage != "" {
+		url += fmt.Sprintf("%s%sgb", appendStr, scrapSettings.Storage)
+	}
+
+	if scrapSettings.Inches != "" {
+
+		floatValue, err := strconv.ParseFloat(scrapSettings.Inches, 64)
+		if err != nil {
+			fmt.Println("Error parsing float:", err)
+		}
+		intValue := int(floatValue)
+		result := strconv.Itoa(intValue)
+
+		url += fmt.Sprintf("%s%s", appendStr, result)
+	}
+
+	if scrapSettings.Processor != "" {
+		url += fmt.Sprintf("%s%s", appendStr, scrapSettings.Processor)
+	}
+
+	if scrapSettings.MinPrice != "" || scrapSettings.MaxPrice != "" {
+		if scrapSettings.MinPrice == "" {
+			scrapSettings.MinPrice = "0"
+		}
+
+		if scrapSettings.MaxPrice == "" {
+			scrapSettings.MaxPrice = "0"
+		}
+	}
+
+	// Se crea y devuelve la url que finalmente se va a scrapear
+	visitUrl := url
+
+	return visitUrl
 }
 
 // Scrapea datos de fravega, a partir de una url, y devuelve los productos
